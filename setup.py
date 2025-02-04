fa3e86ef29c61ebfa8f0c349baa08c9dc69f88dc
miguelgfierro <miguelgfierro@users.noreply.github.com>
2025-02-04 12:48:16 +0100

    update numpy and pandas version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ version = version_data.get("__version__", "0.0")
 
 install_requires = [
     "numpy>=1.19",  # 1.19 required by tensorflow 2.6
-    "pandas>1.0.3,<2",
+    "pandas>2,<3",
     "ipykernel>=4.6.1,<7",
     "jupyter>=1,<2",
     "black>=18.6b4,<21",
